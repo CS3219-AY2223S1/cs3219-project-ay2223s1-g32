bc04abcf9954364320f147591291c0f1cc185587
Rachel <rgabelarde@gmail.com>
2022-09-28 23:55:09 +0800

    Add interim homepage

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,15 @@
+import {
+    Box,
+    Typography,
+} from "@mui/material";
+
+function HomePage() {
+
+    return (
+        <Box display={"flex"} flexDirection={"column"} width={"30%"}>
+        <Typography> test </Typography>
+        </Box>
+    )
+}
+
+export default HomePage;
