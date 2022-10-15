b790d71429c692663cd104300c63087a472c993d
oeiyiping <oeiyiping@gmail.com>
2022-10-15 17:01:23 +0800

    Implement question service server index

---
+++ question-service/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const cors = require('cors')
+const { getAllQuestions, getQuestion, getRandomQuestion } = require('./controller/question-controller')
+
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors()) // config cors so that front-end can use
+app.options('*', cors())
+
+const router = express.Router()
+router.get('/', (_, res) => res.send('Hello World from question-service'))
+app.use(router)
+
+const questionRouter = express.Router()
+
+questionRouter
+  .get('/', getAllQuestions)
+  .get('/id', getQuestion)
+  .get('/random', getRandomQuestion)
+
+
+app.use('/api/question', questionRouter).all((_, res) => {
+    res.setHeader('content-type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+})
+
+const port = 8002
+app.listen(port, () => console.log(`question-service listening on port ${port}`));
