2b5cd43f0d91e09a8a86048cb196ffc0c3ebea0f
oeiyiping <oeiyiping@gmail.com>
2022-11-04 20:47:09 +0800

    Add DB object CRUD methods as repository methods

---
+++ history-service/repository/repository.js
@@ -0,0 +1,53 @@
+const dotenv = require('dotenv').config()
+const mongoose = require("mongoose")
+const History = require('../model/history-model')
+
+let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const getAllHistories = async () => { 
+  const histories = await History.find({})
+  return histories
+}
+
+const getHistory = async (id) => { 
+  const history = await History.findById(id)
+  return history
+}
+
+const getUserHistory = async (userId) => {
+  const history = await History.find({ user: userId })
+  return history
+}
+
+const createHistory = async (user, collaborator, question, content) => {
+  const newHistory = await History.create({
+    user,
+    collaborator,
+    question,
+    content
+  })
+
+  return newHistory
+}
+
+const deleteHistory = async (id) => {
+  await History.findByIdAndDelete(id);
+}
+
+const deleteUserHistory = async (userId) => {
+  await History.deleteMany({ user: userId });
+}
+
+module.exports = { 
+  getAllHistories, 
+  getHistory, 
+  getUserHistory, 
+  createHistory,
+  deleteHistory,
+  deleteUserHistory
+}
