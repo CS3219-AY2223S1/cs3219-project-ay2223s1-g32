e8dffffa7fd5fcf922698ece6802eac33a111ddd
Rachel <rgabelarde@gmail.com>
2022-11-09 05:39:16 +0800

    Add matching timeout/errors dialogs

--- frontend/src/screens/MatchingPage.js
+++ frontend/src/screens/MatchingPage.js
@@ -1,4 +1,11 @@
 import * as React from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 import Box from "@mui/material/Box";
 import Fade from "@mui/material/Fade";
 import Button from "@mui/material/Button";
@@ -10,6 +17,9 @@ import TopNavBar from '../components/TopNavBar.js';
 
 export default function MatchingPage() {
   const [query, setQuery] = React.useState("idle");
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+  const [dialogMsg, setDialogMsg] = React.useState("");
+  const [dialogTitle, setDialogTitle] = React.useState("")
   const timerRef = React.useRef();
   const navigate = useNavigate();
   const location = useLocation();
@@ -33,15 +43,17 @@ export default function MatchingPage() {
   socket.on("SuccessMatched", (response, roomID) => {
     console.log(response); // false
     setRoomID(roomID);
-    navigate("/matched", { state : { difficulty: difficulty, roomID: roomID }});
+    navigate("/matched", { state: { difficulty: difficulty, roomID: roomID } });
   });
 
   socket.on("Error", (response) => {
     console.log(response); // false
+    setErrorDialog("Error finding match.")
   });
 
   socket.on("TimeoutError", (response) => {
     console.log(response); // false
+    setErrorDialog("Please try again, it took too long :(")
   });
 
   React.useEffect(
@@ -77,43 +89,64 @@ export default function MatchingPage() {
     }, 30000);
   };
 
+  const closeDialog = () => setIsDialogOpen(false)
+
+  const setErrorDialog = (msg) => {
+    setIsDialogOpen(true)
+    setDialogTitle('Error')
+    setDialogMsg(msg)
+  }
+
+
   return (
     <>
-    <TopNavBar  />
-    <Box
-      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <Box sx={{ height: 40 }}>
-        {query === "success" ? (
-          <Typography>Success!</Typography>
-        ) : (
-          <Fade
-            in={query === "progress"}
-            style={{
-              transitionDelay: query === "progress" ? "800ms" : "0ms",
-            }}
-            unmountOnExit
+      <TopNavBar />
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Box sx={{ height: 40 }}>
+          <Dialog
+            open={isDialogOpen}
+            onClose={closeDialog}
           >
-            <CircularProgress />
-          </Fade>
-        )}
+            <DialogTitle>{dialogTitle}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>{dialogMsg}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={closeDialog}>Close</Button>
+            </DialogActions>
+          </Dialog>
+          {query === "success" ? (
+            <Typography>Success!</Typography>
+          ) : (
+            <Fade
+              in={query === "progress"}
+              style={{
+                transitionDelay: query === "progress" ? "800ms" : "0ms",
+              }}
+              unmountOnExit
+            >
+              <CircularProgress />
+            </Fade>
+          )}
+        </Box>
+        <Button onClick={handleClickQuery} sx={{ m: 2 }}>
+          {query !== "idle" ? "Stop Matching" : "Start Matching"}
+        </Button>
+        <div>
+          <Box sx={{ height: 40 }}>
+            <Link to="/login">
+              <Button variant="outlined">Go Home</Button>
+            </Link>
+          </Box>
+          <Box sx={{ height: 40 }}>
+            <Link to="/selectdifficulty">
+              <Button variant="outlined">Change difficulty</Button>
+            </Link>
+          </Box>
+        </div>
       </Box>
-      <Button onClick={handleClickQuery} sx={{ m: 2 }}>
-        {query !== "idle" ? "Stop Matching" : "Start Matching"}
-      </Button>
-      <div>
-        <Box sx={{ height: 40 }}>
-          <Link to="/login">
-            <Button variant="outlined">Go Home</Button>
-          </Link>
-        </Box>
-        <Box sx={{ height: 40 }}>
-          <Link to="/selectdifficulty">
-            <Button variant="outlined">Change difficulty</Button>
-          </Link>
-        </Box>
-      </div>
-    </Box>
     </>
   );
 }
