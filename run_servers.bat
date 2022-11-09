5c0316bd93508fd6abac772fed70641d945c26cf
Jace <jace1711@gmail.com>
2022-11-09 16:56:43 +0800

    Add final report file

--- run_servers.bat
+++
@@ -1,7 +0,0 @@
-node ./user-service/index.js
-node ./matching-service/index.js
-node ./question-service/index.js
-node ./matching-service/server.js
-node ./chatting-service/server/index.js
-node ./history-service/index.js
-node ./frontend/index.js
